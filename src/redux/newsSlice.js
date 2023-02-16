949827e86dd299c2f3d731cbccde8dd76ff10b0c
yanakhorolska <yana.khorolska@protonmail.com>
2023-02-16 17:45:02 +0100

    Update newsSlice.js

--- src/redux/newsSlice.js
+++ src/redux/newsSlice.js
@@ -1,14 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const params = new URLSearchParams({
+  ApiKey: '48c5727fada347ffbc441598f50b3de6',
+  pageSize: 20,
+  page: 1,
+  q: 'css',
+});
+
 export const newsApi = createApi({
   reducerPath: 'news',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://newsapi.org/v2/',
   }),
+
   endpoints: builder => ({
     getNews: builder.query({
-      query: () =>
-        `/everything?apiKey=48c5727fada347ffbc441598f50b3de6&pageSize=20&page=1&q=css`,
+      query: () => `/everything?${params}`,
     }),
   }),
 });
