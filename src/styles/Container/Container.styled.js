01ec240a6b38a0ac467bac8f899d62ac9f25a327
yanakhorolska <yana.khorolska@protonmail.com>
2023-02-19 23:32:04 +0100

    update styles

---
+++ src/styles/Container/Container.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled('div')`
+  margin: ${p => p.theme.space.m.auto};
+  padding-left: ${p => p.theme.space.p.xs};
+  padding-right: ${p => p.theme.space.p.xs};
+  @media ${p => p.theme.media.mobileMax} {
+    max-width: ${p => p.theme.sizes.width.mediaS};
+  }
+  @media ${p => p.theme.media.mobileMin} {
+    width: ${p => p.theme.sizes.width.mediaS};
+  }
+  @media ${p => p.theme.media.tabletDesktop} {
+    width: ${p => p.theme.sizes.width.mediaM};
+    padding-left: ${p => p.theme.space.p.s};
+    padding-right: ${p => p.theme.space.p.s};
+  }
+  @media ${p => p.theme.media.desktop} {
+    width: ${p => p.theme.sizes.width.mediaL};
+    padding-left: ${p => p.theme.space.p.xxs};
+    padding-right: ${p => p.theme.space.p.xxs};
+  } ;
+`;
