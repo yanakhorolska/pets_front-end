60070d1af512f94e45b4fd47102d377a144b34e3
dmitkate <44602492+dmitkate@users.noreply.github.com>
2023-02-19 19:58:03 +0200

    add route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import Logotype from 'components/Logo/logo';
 import { Head, List, Link } from './header.styled';
-
+import { useAuth } from 'hooks/useAuth';
 const Header = () => {
+  const { isLoggedIn } = useAuth();
   return (
     <Head>
       <Logotype />
@@ -11,6 +12,16 @@ const Header = () => {
         <Link to="/notices">Find pet</Link>
         <Link to="/friends">Our friends</Link>
       </List>
+      <>
+        {isLoggedIn ? (
+          <Link to="/user">Account</Link>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Registration</Link>
+          </>
+        )}
+      </>
     </Head>
   );
 };
