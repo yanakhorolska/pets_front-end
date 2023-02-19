6240fb6ca5d93f65c1df57d35907d805c8c98c63
Semestotell <kozlov.k.9527@gmail.com>
2023-02-19 11:12:04 +0100

    theme

--- src/styles/theme/theme.js
+++ src/styles/theme/theme.js
@@ -1,1 +1,71 @@
-export const theme = {};
+export const theme = {
+    colors: {
+        black: "#000",
+        white: "#fff",
+        background: "#FDF7F2",
+        accent: "#F59256",
+
+        logo: "#111111",
+
+        text: "#111111",
+        primaryText: "#111111",
+        navBarText: "#181C27",
+        searchText: "#535353",
+        textRegister: "#3091EB",
+        primaryBtnText: "#111111",
+        secondatyBtnText: "#FFFFFF",
+
+        hoverBtn: "#FF6101",
+
+        shadowCard: "rgba(49, 21, 4, 0.07)",
+
+        greyText: "rgba(17, 17, 17, 0.6)",
+        inputModal: "rgba(245, 146, 86, 0.5)",
+    },
+
+    space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+    fonts: {
+        body: "Manrope', sans-serif",
+        logo: "Poppins', sans-serif",
+    },
+
+    fontSizes: {
+        xs: "12px",
+        s: "14px",
+        m: "16px",
+        l: "32px",
+        xl: "64px",
+    },
+
+    fontWeights: {
+        default: 400,
+        heading: 500,
+        bold: 700,
+    },
+
+    lineHeights: {
+        main: 1.4,
+    },
+
+    borders: {
+        none: "none",
+        normal: "1px solid",
+        inputModal: "2px solid",
+    },
+
+    radius: {
+        none: "0",
+        btn: "40px",
+        inputModal: "20px",
+        round: "50%",
+    },
+
+    media: {
+        mobile: "(min-width: 320px)",
+        tablet: "(max-width: 1279px) and (min-width: 768px)",
+        tabletDesktop: "(min-width: 768px)",
+        desktop: "(min-width: 1280px)",
+    },
+};
+
