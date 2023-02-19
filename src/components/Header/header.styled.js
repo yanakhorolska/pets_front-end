d4d274b1c1ea032e255a93b8005ff75775723756
dmitkate <44602492+dmitkate@users.noreply.github.com>
2023-02-19 16:27:28 +0200

    1

---
+++ src/components/Header/header.styled.js
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled'
+import { NavLink } from "react-router-dom"
+
+export const Head = styled.header`
+    display: flex;
+    flex-diracion:row;
+    padding: 20px 0;
+    align-items: center;
+`;
+export const List = styled.nav`   
+    display: flex;
+    flex-diracion:row;  
+`;
+export const Link = styled(NavLink)`
+    font-family: 'Manrope', sans-serif;
+    margin-right: 80px;
+    text-decoration: none; 
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 0.74;
+    letter-spacing: 0.04em;
+    color:#111111;
+    &.active {
+    text-decoration-line: underline;
+    color: #F59256;
+    }
+`;
