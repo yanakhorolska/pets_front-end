79983ffd77aa12342462604a4a22aa3496b31b29
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2023-02-20 18:27:15 +0200

    add some styles and logic of button 'add pet'

--- src/components/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/AddNoticeButton/AddNoticeButton.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-// import { useModal } from 'hooks';
-// import Modal from 'components/Modal';
+// тут мав би бути імпорт глобальної  модалки (ModalAddsPet). Хто її робить?
 
-// import { ModalAddUserNotice } from 'components/ModalAddUserNotice';
+// тут треба прописати імпорт хука ЮзМодал.(наприклад, import { useModal } from 'hooks';) Хто її пише?
+
+// export const getIsLogged = state => state.auth.isLogged;
 
 const AddNoticeButton = () => {
-  //   const isLogged = useSelector(state => state.users.isLogged);
+  const isLogged = useSelector(state => state.auth.isLogged);
+  const navigate = useNavigate();
+
+  // logic of opening and closing modal window
+
+  //   const { isModalOpen, closeModal, toggleModal } = useModal();
+
+  const buttonSwitch = () => {
+    if (isLogged) {
+      //   toggleModal(); логіка перемикання модалки
+      return;
+    } else {
+      navigate('/login');
+      alert('Please login first');
+      return;
+    }
+  };
+
+  // --- example how modal window will be render --- //
+
+  //   {
+  //     isModalOpen && (
+  //       <ModalGlobalEl onCloseModal={closeModal}>
+  //         {<ModalAddNotice onCloseModal={closeModal} />}
+  //       </ModalGlobalEl>
+  //     );
+  //   }
 
   return (
     <>
       <div>
-        <button type="button">Add pet</button>
+        <button type="button" onClick={buttonSwitch}>
+          Add pet
+        </button>
       </div>
     </>
   );
