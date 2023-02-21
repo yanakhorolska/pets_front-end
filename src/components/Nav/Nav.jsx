93d7f11f7dd73ade4e252b7610a60ba95a711f48
dmitkate <44602492+dmitkate@users.noreply.github.com>
2023-02-21 19:13:27 +0200

    add adaptive header

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,13 @@
-import { List, Link } from './nav.styles';
+import { List, Link } from './nav.styled';
+
+import { ToggleButton } from 'components/ToggleButton/ToggleButton';
 const Nav = () => {
   return (
     <List>
       <Link to="/news">News</Link>
       <Link to="/notices">Find pet</Link>
       <Link to="/friends">Our friends</Link>
+      <ToggleButton />
     </List>
   );
 };
