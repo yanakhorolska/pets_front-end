53c92f86f693e22924cbb5c7ad50e80519b2d818
yanakhorolska <yana.khorolska@protonmail.com>
2023-02-23 21:23:29 +0100

    auth

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,4 @@
 export const getToken = state => state.auth.token;
 export const getIsLogged = state => state.auth.isLogged;
-
-export const getUser = state => state.auth.user
+export const getUser = state => state.auth.user;
 export const getUserAvatarURL = state => state.auth.user.avatarURL;
-
-
