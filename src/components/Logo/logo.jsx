b656d968566fa129f49819381c249732747376ad
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2023-02-23 20:50:50 +0200

    +

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,9 +1,12 @@
 import { Logo } from './logo.styled';
+import { Link } from 'react-router-dom';
 
 const Logotype = () => {
   return (
     <Logo>
-      pe<span style={{ color: '#F59256' }}>t</span>ly
+      <Link to="/">
+        pe<span style={{ color: '#F59256' }}>t</span>ly
+      </Link>
     </Logo>
   );
 };
