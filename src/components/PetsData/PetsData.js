81cdf8b2ad8359d92b96913613277998265f0937
OlgaBorovik <jkz8686@gmail.com>
2023-02-24 10:11:13 +0200

    добавлена кнопка

--- src/components/PetsData/PetsData.js
+++ src/components/PetsData/PetsData.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PetsList from "../PetsList/PetsList.jsx"
+import AddButton from 'components/AddButton';
+import {AddPet} from '../ModalAddsPet/AddPet'
 
 const PetsData = () => {
   return (
       <div>
           <p>My Pets</p>
-          PetsData
+          <AddButton component={AddPet} />
           <PetsList />
       </div>
   )
--- src/components/UserDataItem/UserDataItem.jsx
+++ src/components/UserDataItem/UserDataItem.jsx
@@ -1,45 +1,57 @@
-import React from 'react'
-import { Formik, Form, Field } from 'formik'
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useUpdateUserMutation } from '../../redux/fetchUser';
 import { setUpdatedUser } from 'redux/authSlice';
-// import { getToken } from 'redux/selectors';
 
-const UserDataItem = ({ initialValues, name, value }) => {
+const UserDataItem = ({ initialValues, text, name, placeholder }) => {
   const [disabled, setDisabled] = useState(true);
-  
   const dispatch = useDispatch();
-
   const [updateUser] = useUpdateUserMutation();
+  const [inputeData, setInputData] = useState('');
 
   const handleSubmit = async values => {
-  try {
+    console.log(values);
+    values[text] = inputeData;
+    try {
       if (!disabled);
-    const user = await updateUser({values}).unwrap();
+      const user = await updateUser({ values }).unwrap();
       dispatch(setUpdatedUser(user));
-      console.log("User after update", user)
-      
+      setInputData('');
+      setDisabled(true);
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-    
   };
-  
+
+  const changeInput = e => {
+    const data = e.currentTarget.value;
+    setInputData(data);
+    console.log(data);
+  };
+
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <Form>
         <label>{name}: </label>
-        <Field type="text" name={name} disabled={disabled} ></Field>
-        <button type="button" onClick={() => setDisabled(!disabled)} >Edit</button>
-        <button type="submit" disabled={disabled}>Submit</button>       
-        
+        <Field
+          type="text"
+          onChange={changeInput}
+          value={inputeData}
+          name={name}
+          placeholder={placeholder}
+          disabled={disabled}
+        />
+        <button type="button" onClick={() => setDisabled(!disabled)}>
+          Edit
+        </button>
+        <button type="submit" disabled={disabled}>
+          Submit
+        </button>
       </Form>
-      
-</Formik>
-    
-  )
-}
+    </Formik>
+  );
+};
 
-export default UserDataItem
+export default UserDataItem;
